d18816591269c394623dbf834a5a38864740f2fc
Keith Ma <keithfma@gmail.com>
2018-06-06 02:02:16 -0400

    Complete LiDAR tile download script

--- data/noaa_lidar_sandy/get.py
+++ data/noaa_lidar_sandy/get.py
@@ -2,38 +2,22 @@
 Utility for selecting and downloading LiDAR data from https://coast.noaa.gov 
 """
 
-# import shapefile
 from osgeo import ogr
-from shutil import copyfile
+import os
+import wget
 
-# constants
-BND_SHP_FILE = '../shp/ma_community_bnd/data/TOWNSSURVEY_POLYM.shp'
-STUDY_TOWNS = ['BROOKLINE', 'BOSTON', 'NEWTON', 'SOMERVILLE', 'WATERTOWN',
-                    'BELMONT', 'CAMBRIDGE']
-STUDY_SHP = '../shp/ma_community_bnd/munged/study_area.shp'
+# gather list of tile urls
+shp = ogr.Open('../noaa_lidar_index/deriv/study_area.shp')
+layer = shp.GetLayer()
+urls = [feature['URL'].strip() for feature in layer]
 
-# TODO: load MA town boundaries and extract study area
+# create dist directory
+if not os.path.isdir('dist'):
+    os.makedirs('dist')
 
-# bnd_shp = shapefile.Reader(BND_SHP_FILE)
-# study_objs = []
-# for shp in bnd_shp.shapeRecords():
-#     if shp.record[0] in STUDY_AREA_TOWNS:
-#         study_area_objs.append(shp)
-# 
-# print(len(STUDY_AREA_TOWNS), len(study_area_objs))
-
-bnd_shp = ogr.Open(BND_SHP_FILE)
-bnd_layer = bnd_shp.GetLayer()
-study_poly = ogr.Geometry(ogr.wkbPolygon)
-for feature in bnd_layer:
-    if feature['TOWN'] in STUDY_AREA_TOWNS:
-        geom = feature.GetGeometryRef()
-        study_poly = study_poly.Union(geom)
-
-# TODO: load LiDAR (NOAA Post-Sandy) tiles
-
-# TODO: get list of all tiles in study area
-
-# TODO: download tiles one-at-a-time
+# download tiles one-at-a-time
+for ii, url in enumerate(urls):
+    print('Downloading tile {}/{} from {}'.format(ii, len(urls), url))
+    wget.download(url, out='dist')
 
 # TODO: unpack and clean up

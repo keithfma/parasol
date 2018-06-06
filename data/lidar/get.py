ed258763c751aef5129214d7c849b5e025fdf638
Keith Ma <keithfma@gmail.com>
2018-06-06 00:31:45 -0400

    First attempt at generating a study area shp file

--- data/lidar/get.py
+++ data/lidar/get.py
@@ -2,7 +2,23 @@
 Utility for selecting and downloading LiDAR data from https://coast.noaa.gov 
 """
 
+import shapefile
+
+# constants
+BND_SHP_FILE = '../shp/ma_community_bnd/data/TOWNSSURVEY_POLYM.shp'
+STUDY_AREA_TOWNS = ['BROOKLINE', 'BOSTON', 'NEWTON', 'SOMERVILLE', 'WATERTOWN',
+                    'BELMONT', 'CAMBRIDGE']
+
 # TODO: load MA town boundaries and extract study area
+
+bnd_shp = shapefile.Reader(BND_SHP_FILE)
+study_objs = []
+for shp in bnd_shp.shapeRecords():
+    if shp.record[0] in STUDY_AREA_TOWNS:
+        study_area_objs.append(shp)
+
+print(len(STUDY_AREA_TOWNS), len(study_area_objs))
+
 
 # TODO: load LiDAR (NOAA Post-Sandy) tiles
 

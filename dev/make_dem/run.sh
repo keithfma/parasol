ecadbf05084039d2697a1b21140b0ef6aa715afe
Keith Ma <keithfma@gmail.com>
2018-06-10 12:53:27 -0400

    reprojection-only pipeline

--- dev/make_dem/run.sh
+++ dev/make_dem/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IN_FILE=/home/keith/prj/parasol/repo/data/noaa_lidar_sandy/dist/20131208_usgspostsandy_19TCG120875.laz
-OUT_FILE=./out
+IN_FILE=./in.laz
+OUT_FILE=./out.laz
 
-pdal pipeline pipeline.json --readers.las.filename=$IN_FILE --writers.las.filename=$OUT_FILE.las
+pdal pipeline pipeline.json --readers.las.filename=$IN_FILE --writers.las.filename=$OUT_FILE

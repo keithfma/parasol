d3a11906511c66751b7cb46602c0d55d0736f64d
Keith Ma <keithfma@gmail.com>
2018-06-13 17:37:22 -0400

    Started new approach to MVP app initialization

---
+++ mvp/fetch_data.py
@@ -0,0 +1,16 @@
+"""
+Utilities for retrieving data for parasol app
+"""
+
+def get_lidar():
+    """
+    Download LiDAR tiles for the Parasol study area
+    """
+    raise NotImplementedError
+
+
+def get_osm():
+    """
+    Download OpenStreetMaps data for the Parasol study area
+    """
+    raise NotImplementedError

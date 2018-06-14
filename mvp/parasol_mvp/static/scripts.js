1fd6d0d5f50a5577e5ed9a3df5bc38e038cd8f0e
Keith Ma <keithfma@gmail.com>
2018-06-14 19:55:38 -0400

    Working slider!

--- mvp/parasol_mvp/static/scripts.js
+++ mvp/parasol_mvp/static/scripts.js
@@ -2,6 +2,7 @@
 // SEE: http://bl.ocks.org/bentrm/5aaedf8f2bd9361e280d
 
 // script globals
+var shadeBbox = [[42.3343660, -71.0825971], [42.3620201, -71.0453125]];
 var shadeLayer;
 var shadeUrls = [];
 var lon0;
@@ -25,7 +26,7 @@ $(document).ready(function() {
     var initShadeIdx = 40; // 10 AM
     
     // update slider properties
-    var slider = $('.slider')[0];
+    var slider = $('#timeSlider')[0];
     slider.min = "0";
     slider.max = shadeUrls.length.toString();
     slider.value = initShadeIdx;
@@ -33,7 +34,7 @@ $(document).ready(function() {
     // init the map object
     var map = L.map('parasol-map', {
         center: [42.3481931, -71.0639548],
-        zoom: 13
+        zoom: 15
     });
 
     // add basemap
@@ -43,21 +44,25 @@ $(document).ready(function() {
         attribution: osmAttrib
     }).addTo(map); // this will be our active base layer on startup
 
-    console.log(shadeUrls);
-
     // add initial shade map
-    var image = L.imageOverlay(
-        shadeUrls[initShadeIdx],
-        [[42.3343660, -71.0825971], [42.3620201, -71.0453125]], {
-        opacity: .6
-    }).addTo(map);
+    shadeLayer = L.imageOverlay(shadeUrls[initShadeIdx], shadeBbox, {opacity: .9})
+    shadeLayer.addTo(map);
 
     // init a map scale
     L.control.scale().addTo(map);
 
+    // init a simple layer switcher with overlays an mutual exclusive base layers
+    var baseLayers = {"Map": osm}
+    var overlays = {"Shade": shadeLayer};
+    L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
+
     // add event listener for slider
     slider.onchange= function() {
-        console.log('SLIDER = ' + slider.value);
+        var shadeIdx = parseInt($('#timeSlider')[0].value);
+        shadeLayer.remove();
+        shadeLayer = L.imageOverlay(shadeUrls[shadeIdx], shadeBbox, {opacity: .9})
+        shadeLayer.addTo(map);
+        console.log('Updated shade map to idx: ' + shadeIdx.toString() + ", url: " + shadeUrls[shadeIdx]); 
     };
 
     // // listen to click events to show a popup window

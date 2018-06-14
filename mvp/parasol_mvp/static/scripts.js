f1bbbcc529e8cc0a25d476ce52a90d536be6a03d
Keith Ma <keithfma@gmail.com>
2018-06-14 16:25:21 -0400

    Add one layer only, and generate a list of urls

--- mvp/parasol_mvp/static/scripts.js
+++ mvp/parasol_mvp/static/scripts.js
@@ -1,6 +1,10 @@
 // SEE: https://sandbox.idre.ucla.edu/sandbox/general/zoomable-image-with-gdal-and-leaflet
 // SEE: http://bl.ocks.org/bentrm/5aaedf8f2bd9361e280d
 
+var shadeLayer;
+var shadeUrls = [];
+
+
 // init - run on page load
 $(document).ready(function() {
     
@@ -18,25 +22,22 @@ $(document).ready(function() {
         attribution: osmAttrib
     }).addTo(map); // this will be our active base layer on startup
 
-    // // an image overlay that will be added to the overlays of a layer switcher
-    // var image = L.imageOverlay(
-    //     '/img/top.png',
-    //     [[42.3343660, -71.0825971], [42.3620201, -71.0453125]], {
-    //     opacity: .8
-    // }).addTo(map);
-
-    // loop over 15 minute time intervals, add all layers
+    // Generate list of all shade image urls
     for (let tt = 0; tt <= 2400; tt += 25) {
         // generate image file path
         var tstr = tt.toString().padStart(4, "0");
-        var img_file = '/img/shade_' + tstr.substring(0,2) + '.' +tstr.substring(2) + '.png';
-        // add image overlay
-        var image = L.imageOverlay(
-            img_file,
-            [[42.3343660, -71.0825971], [42.3620201, -71.0453125]], {
-            opacity: .8
-        }).addTo(map);
+        shadeUrls.push('/img/shade_' + tstr.substring(0,2) + '.' +tstr.substring(2) + '.png');
     } 
+    console.log(shadeUrls);
+
+    // TODO: set initial layer by current time
+
+    // an image overlay that will be added to the overlays of a layer switcher
+    var image = L.imageOverlay(
+        '/img/shade_10.00.png',
+        [[42.3343660, -71.0825971], [42.3620201, -71.0453125]], {
+        opacity: .6
+    }).addTo(map);
 
     // init a map scale
     L.control.scale().addTo(map);

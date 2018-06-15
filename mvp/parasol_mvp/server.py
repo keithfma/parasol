5a2c53c04411f2f307db68ab2cf55915fb6ca23a
Keith Ma <keithfma@gmail.com>
2018-06-15 00:14:57 -0400

    Compute route as geojson

--- mvp/parasol_mvp/server.py
+++ mvp/parasol_mvp/server.py
@@ -64,9 +64,16 @@ def route():
     cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
                 (lon1, lat1))
     end_id = cur.fetchone()[0]
+
+    # compute route, return GeoJSON for edges
+    sql ='SELECT gid AS id, source, target, length AS cost, the_geom FROM ways'
+    cur.execute(f"SELECT ST_AsGeoJSON(ST_UNION(ways.the_geom)) FROM pgr_dijkstra('{sql}', %s, %s, directed := false) LEFT JOIN ways ON (edge = gid);",
+                (start_id, end_id))
+    geojson = cur.fetchone()[0]
     
     # DEBUG
     print(start_id, end_id)
+    print(geojson)
 
     return flask.Response(status=501)
 

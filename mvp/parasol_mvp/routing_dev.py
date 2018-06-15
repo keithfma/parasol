89f9162b29d12d95489413ffe891bfd4dbb4a5c2
Keith Ma <keithfma@gmail.com>
2018-06-14 23:52:39 -0400

    Working route as geojson

--- mvp/parasol_mvp/routing_dev.py
+++ mvp/parasol_mvp/routing_dev.py
@@ -12,16 +12,21 @@ cur = conn.cursor()
 cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
             (-71.04143, 42.35126))
 start_id = cur.fetchone()[0]
-print(start_id)
 
 cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
             (-71.06143, 42.35126))
 end_id = cur.fetchone()[0]
-print(end_id)
 
 # compute route
 sql ='SELECT gid AS id, source, target, length AS cost FROM ways'
 cur.execute(f"SELECT * FROM pgr_dijkstra('{sql}', %s, %s, directed := false);",
             #(4847, 9270))
             (start_id, end_id))
-print(cur.fetchall())
+route = cur.fetchall()
+
+
+# compute route, return GeoJSON for edges
+sql ='SELECT gid AS id, source, target, length AS cost, the_geom FROM ways'
+cur.execute(f"SELECT ST_AsGeoJSON(ST_UNION(ways.the_geom)) FROM pgr_dijkstra('{sql}', %s, %s, directed := false) LEFT JOIN ways ON (edge = gid);",
+            (start_id, end_id))
+route_geojson = cur.fetchone()[0]

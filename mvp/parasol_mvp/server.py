cda496db171a928e98f838c197af85c32fb6966e
Keith Ma <keithfma@gmail.com>
2018-06-14 20:31:07 -0400

    Added stub for route endpoint

--- mvp/parasol_mvp/server.py
+++ mvp/parasol_mvp/server.py
@@ -23,10 +23,26 @@ def main():
     """Main Parasol user interface"""
     return flask.render_template('index.html')
 
+
 @app.route('/img/<img_name>', methods=['GET'])
 def img_file(img_name):
+    """Serve files in static/img by name"""
     filename = resource_filename('parasol_mvp', os.path.join('static', 'img', img_name))
     return flask.send_file(filename, mimetype='image/png')
+
+
+@app.route('/route', methods=['GET'])
+def route():
+    """
+    Compute route between specified start and end points
+
+    Parameters (URL query string):
+        lat0, lon0 = floats, start point latitude, longitude
+        lat1, lon1 = floats, end point latitude, longitude
+
+    Returns: optimal route as geoJSON
+    """
+    return flask.Response(status=501)
 
 
 # command line ---------------------------------------------------------------

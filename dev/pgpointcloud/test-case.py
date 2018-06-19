0209bb02cb108dab0f66580dfeb742afe54609b7
Keith Ma <keithfma@gmail.com>
2018-06-19 01:09:19 -0400

    Simple test of grid creation, passes

--- dev/pgpointcloud/test-case.py
+++ dev/pgpointcloud/test-case.py
@@ -6,15 +6,19 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 # # ingest tiny dataset, save results to viewable file
-parasol.lidar.create_db(True)
-sample_file = 'subset.laz'
+# parasol.lidar.create_db(True)
+# sample_file = 'subset.laz'
 maxx = 315118.39                                                                                                                                                                                
 minx = 315000                                                                                                                                                                                   
 maxy = 4686154.02                                                                                                                                                                               
 miny = 4686000                                                                                                                                                                                
-parasol.lidar.ingest(sample_file)
-pts = parasol.lidar.retrieve(minx, maxx, miny, maxy)
+# parasol.lidar.ingest(sample_file)
+# pts = parasol.lidar.retrieve(minx, maxx, miny, maxy)
 # pts = parasol.lidar.retrieve()
+
+# experiment with gridding
+out = parasol.lidar.grid_points(minx, maxx, miny, maxy)
+
 
 # # read whole current dataset
 # pts = parasol.lidar.retrieve()

5b962f71b34b0632b97af072f538d4d8dc3a75d6
Keith Ma <keithfma@gmail.com>
2018-06-20 19:21:21 -0400

    Worked through shade map generation with GRASS interactive, notes in test.py

---
+++ dev/shade-raster/test.py
@@ -0,0 +1,37 @@
+"""Explore generating and ingesting shade rasters"""
+
+import parasol
+
+# extract a subset and write out as geotiff
+x_min = 334499.6937
+x_max = 335000
+y_min = 4694999.905       
+y_max = 4695500
+
+parasol.raster.retrieve_geotiff('delete_me', x_min, x_max, y_min, y_max)
+
+# r.import input=/home/keith/prj/parasol/repo/dev/shade-raster/delete_me_surface.tif output=delete_me_surface
+# g.region raster=delete_me_surface@parasol-scratch 
+# r.slope.aspect --overwrite elevation=delete_me_surface@parasol-scratch slope=slope aspect=aspect
+# r.latlong input=delete_me_surface@parasol-scratch output=lat              
+# r.latlong -l input=delete_me_surface@parasol-scratch output=lon                 
+# r.sun.hourly  elevation=delete_me_surface@parasol-scratch aspect=aspect@parasol-scratch slope=slope@parasol-scratch start_time=12 end_time=13 time_step=0.5 day=150 year=2018 glob_rad_basename=radiation
+
+# import os
+# import subprocess
+# 
+# os.environ['GISBASE'] = '/usr/lib/grass74'
+# os.environ['GISRC'] = '/home/keith/.grass7/rc'
+# 
+# for tt in range(0, 2401, 25):
+#     num_str = '{:04d}'.format(tt)
+#     time_str = num_str[0:2] + '.' + num_str[2:]
+#     layer_name = f'top_rad_{time_str}@mvp'
+#     file_name = f'/home/keith/.parasol_mvp/lidar/rad/rad_{time_str}.tif'
+#     subprocess.run(['grass74', '/home/keith/.grassdata/parasol-dev/mvp', '--exec', 'r.out.gdal', f'input={layer_name}', f'output={file_name}', 'format=GTiff'])
+
+# # show matching raster and vector maps but do not delete yet (as verification)
+# g.remove type=raster,vector pattern="tmp_*"
+# 
+# # actually delete the matching raster and vector maps
+# g.remove -f type=raster,vector pattern="tmp_*"

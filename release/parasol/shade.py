a98c72d0b4d233f060a36264a97eb269d4be075c
Keith Ma <keithfma@gmail.com>
2018-06-20 20:03:14 -0400

    Cleaned up the layer export tool

--- release/parasol/shade.py
+++ release/parasol/shade.py
@@ -16,11 +16,35 @@ logger = logging.getLogger(__name__)
 RADIATION_LAYER_PREFIX = 'radiation'
 
 
-def layers_to_geotiff(dest_dir):
-    for tt in range(0, 2401, 100):
+def sun_base_names(start_hour, stop_hour, interval):
+    """
+    Return list of time strings, as used by GRASS sun.hourly
+
+    Arguments:
+        start_hour: integer, range 0-24
+        stop_hour: integer, range 0-24
+        interval: float, decimal hour
+    
+    Return: list of strings
+    """
+    base_names = []
+    for tt in range(start_hour*100, stop_hour*100 + 1, int(interval*100)):
         hour = '{:02d}'.format(tt // 100)
         minute = '{:02d}'.format(tt % 100)
-        base_name = f'{RADIATION_LAYER_PREFIX}_{hour}.{minute}'
+        base_names.append(f'{RADIATION_LAYER_PREFIX}_{hour}.{minute}')
+    return base_names
+
+
+def layers_to_geotiff(dest_dir):
+    """
+    Write out GRASS radiation layers to local geotiff files
+
+    Arguments:
+        dest_dir: string, destination folder to save files to
+
+    Returns: Nothing
+    """
+    for base_name in sun_base_names(0, 24, 1):
         layer_name = f'{base_name}@{GRASS_MAPSET}'
         file_name = os.path.join(dest_dir, f'{base_name}.tif')
         logger.info(f'Saving "{layer_name}" as "{file_name}"')

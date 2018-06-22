407e6fffed771e0bcc97ae4fde755cf8867d31b5
Keith Ma <keithfma@gmail.com>
2018-06-22 00:55:09 -0400

    Got routing working again, some manual steps

--- release/parasol/server.py
+++ release/parasol/server.py
@@ -7,10 +7,12 @@ import argparse
 import os
 from pkg_resources import resource_filename
 import psycopg2
+from pdb import set_trace
 
 
 # constants
-PSQL_DB = 'parasol'
+# TODO: move to config.json
+PSQL_DB = 'parasol-osm'
 PSQL_USER = 'keith'
 
 # create app
@@ -27,44 +29,44 @@ def main():
     return flask.render_template('index.html')
 
 
-# @app.route('/route', methods=['GET'])
-# def route():
-#     """
-#     Compute route between specified start and end points
-# 
-#     Parameters (URL query string):
-#         lat0, lon0 = floats, start point latitude, longitude
-#         lat1, lon1 = floats, end point latitude, longitude
-# 
-#     Returns: optimal route as geoJSON
-#     """
-#     # parse endpoints
-#     lat0 = float(flask.request.args.get('lat0'))
-#     lon0 = float(flask.request.args.get('lon0'))
-#     lat1 = float(flask.request.args.get('lat1'))
-#     lon1 = float(flask.request.args.get('lon1'))
-#    
-#     # connect to Postgresql database 
-#     conn = psycopg2.connect(f"dbname={PSQL_DB} user={PSQL_USER}")
-#     cur = conn.cursor()
-# 
-#     # find start vertex
-#     cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
-#                 (lon0, lat0))
-#     start_id = cur.fetchone()[0]
-# 
-#     # find end vertex
-#     cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
-#                 (lon1, lat1))
-#     end_id = cur.fetchone()[0]
-# 
-#     # compute route, return GeoJSON for edges
-#     sql ='SELECT gid AS id, source, target, length AS cost, the_geom FROM ways'
-#     cur.execute(f"SELECT ST_AsGeoJSON(ST_UNION(ways.the_geom)) FROM pgr_dijkstra('{sql}', %s, %s, directed := false) LEFT JOIN ways ON (edge = gid);",
-#                 (start_id, end_id))
-#     geojson = cur.fetchone()[0]
-# 
-#     return flask.Response(status=200, response=geojson, mimetype='application/json')
+@app.route('/route', methods=['GET'])
+def route():
+    """
+    Compute route between specified start and end points
+
+    Parameters (URL query string):
+        lat0, lon0 = floats, start point latitude, longitude
+        lat1, lon1 = floats, end point latitude, longitude
+
+    Returns: optimal route as geoJSON
+    """
+    # parse endpoints
+    lat0 = float(flask.request.args.get('lat0'))
+    lon0 = float(flask.request.args.get('lon0'))
+    lat1 = float(flask.request.args.get('lat1'))
+    lon1 = float(flask.request.args.get('lon1'))
+   
+    # connect to Postgresql database 
+    conn = psycopg2.connect(f"dbname={PSQL_DB} user={PSQL_USER}")
+    cur = conn.cursor()
+    
+    # find start vertex
+    cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
+                (lon0, lat0))
+    start_id = cur.fetchone()[0]
+
+    # find end vertex
+    cur.execute("SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_SetSRID(ST_Point(%s, %s), 4326) LIMIT 1;",
+                (lon1, lat1))
+    end_id = cur.fetchone()[0]
+
+    # compute route, return GeoJSON for edges
+    sql ='SELECT gid AS id, source, target, length AS cost, the_geom FROM ways'
+    cur.execute(f"SELECT ST_AsGeoJSON(ST_UNION(ways.the_geom)) FROM pgr_dijkstra('{sql}', %s, %s, directed := false) LEFT JOIN ways ON (edge = gid);",
+                (start_id, end_id))
+    geojson = cur.fetchone()[0]
+
+    return flask.Response(status=200, response=geojson, mimetype='application/json')
 
 
 # command line ---------------------------------------------------------------

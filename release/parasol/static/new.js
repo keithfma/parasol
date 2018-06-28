6102449543447440dc78214279a044e0483f0142
Keith Ma <keithfma@gmail.com>
2018-06-28 18:20:05 -0400

    Callback gets point locations

--- release/parasol/static/new.js
+++ release/parasol/static/new.js
@@ -1,7 +1,8 @@
 var GeoSearchControl = window.GeoSearch.GeoSearchControl;
 var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
 var map;
-var startSearch;
+var searchProvider;
+var originSearch;
 var destSearch;
 
 
@@ -19,7 +20,7 @@ var greenIcon = new L.Icon({
 // see: https://github.com/smeijer/leaflet-geosearch
 function newSearchControl(text, icon) {
     const search = new GeoSearchControl({
-        provider: new OpenStreetMapProvider(),
+        provider: searchProvider,
         style: "bar",
         autoClose: false,
         showMarker: true,
@@ -35,9 +36,20 @@ function newSearchControl(text, icon) {
     return search
 }
 
+function updateRoute(event) {
+
+    // find marker locations
+    var pts = []
+    map.eachLayer(function(lyr) {
+        if (lyr instanceof L.Marker) {
+            pts.push(lyr.getLatLng());
+        }
+    });
+    console.log(pts);
+}
+
 window.onload = function () {
 
-    // init the map object
     map = L.map('map', {
         center: [42.3481931, -71.0639548],
         zoom: 15,
@@ -51,10 +63,11 @@ window.onload = function () {
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         id: 'mapbox.streets'
     }).addTo(map);
+
+    searchProvider = new OpenStreetMapProvider();
 
     originSearch = newSearchControl(
         "Enter origin address",
@@ -65,5 +78,9 @@ window.onload = function () {
         "Enter destination address",
         greenIcon);
     map.addControl(destSearch); 
+
+    map.on('geosearch/showlocation', updateRoute);
+
+    console.log('Loaded')
 
 };

b84238079b9cdb3d8c2e25670ecbff6604d3931e
Keith Ma <keithfma@gmail.com>
2018-07-09 01:59:17 +0000

    Added a few missing deps

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@
 
 # install packages
 sudo apt -y install postgresql postgresql-client-common python3-pip unzip cmake-curses-gui \
-	postgresql-server-dev-10 libxml2-dev libcunit1-dev autoconf
+	postgresql-server-dev-10 libxml2-dev libcunit1-dev autoconf postgresql-10-postgis-2.4 \
+	postgresql-10-pgrouting
 
 # build and install additional dependencies
 cd deps/laszip
-# ./install.sh
+./install.sh
 
 cd ../pgpointcloud
 ./install.sh

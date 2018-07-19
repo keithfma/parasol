b3a4da81e213f396717ace1f4f7ffe7669b43784
Keith Ma <keithfma@gmail.com>
2018-07-19 11:15:17 +0000

    initial CORS tests work, bumped version

--- pkg/setup.py
+++ pkg/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='parasol',
-    version='0.3.2',
+    version='0.3.3',
     description='Parasol - Optimizing navigation for comfortable outdoor travel',
     packages=find_packages(),
     include_package_data=True,
